9a4d08e14d5b3f0bbff42dd7d838fe2b163fb379
Anil Dhulappanavar <anil@Anils-MacBook-Air.local>
2018-07-30 11:44:31 +0530

    Added reference video's in About Page

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -7,9 +7,23 @@ class AboutPage extends StatelessWidget {
       appBar: new AppBar(
       title: new Text("About Us"),
     ),
-    body: new Center(
-      child:  new Text("Welcome to About Us", style: new TextStyle(fontSize: 35.0))
-    ),
+    body: ListView(
+          children: [
+            new Text("Welcome to About Us",style: new TextStyle(fontSize: 35.0)),
+            new Text("Based on", style: new TextStyle(fontSize: 20.0)),
+            new Text("************* 1 ***************", style: new TextStyle(fontSize: 20.0)),
+            new Text("Bram Vanbilsen - Flutter SDK Tutorial", style: new TextStyle(fontSize: 20.0)),
+            new Text("Building a Beautiful Sliding Side Menu Using a Drawer", style: new TextStyle(fontSize: 20.0)),
+            new Text("https://www.youtube.com/watch?v=WqpV_w6lioA", style: new TextStyle(fontSize: 20.0)),
+            new Text("*************** 2 ***************", style: new TextStyle(fontSize: 20.0)),
+            new Text("Based on", style: new TextStyle(fontSize: 20.0)),
+            new Text("Tensor Programming", style: new TextStyle(fontSize: 20.0)),
+            new Text("Using Cloud Firestore as a Realtime Datastore for CRUD with Dart's Flutter Framework", style: new TextStyle(fontSize: 20.0)),
+            new Text("https://www.youtube.com/watch?v=OJ_u34bD_q8&index=52&list=PLJbE2Yu2zumDqr_-hqpAN0nIr6m14TAsd", style: new TextStyle(fontSize: 20.0)),
+            new Text("************* Thanks ***************", style: new TextStyle(fontSize: 20.0)),
+
+          ],
+        ),
     );
   }
 } 
